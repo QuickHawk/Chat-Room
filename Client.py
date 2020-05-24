8233abe226e729f5cad29602e9dacd19f37980fa
QuiclHawk <devarlaaarya@gmail.com>
2020-05-24 15:38:48 +0530

    Classes and GUI

--- Client.py
+++ Client.py
@@ -1,39 +1,158 @@
 import socket
+import tkinter
 import threading
 
-HOST = "192.168.20.103"
-PORT = 12345
+class Client:
 
-def read_messages(s : socket.socket):
-    '''
-        This function constantly reads if the server recieved 
-        any messages from other clients, if yes, it would be 
-        recieved and printed in the terminal/console.
-    '''
-    while True:
+    HOST = "192.168.20.103"
+    PORT = 12345
 
-        msg = s.recv(1024).decode()
-        print(msg)
+    def __init__(self):
+        '''
+            Initializes the GUI
+        '''
+        
+        self.start_client()
 
-def start_client():
-    '''
-        This function is responsible to connect this pc with
-        the server and be able to messages
-    '''
+        self.screen = tkinter.Tk(screenName="Main Window")
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        canvas = tkinter.Canvas(self.screen, width = 800, height = 600)
+        canvas.pack()
 
-        s.connect((HOST, PORT))
+        self.__startpage(canvas)
 
-        name = input("Enter Name : ")
-        s.send(name.encode())
+        self.screen.mainloop()
 
-        threading._start_new_thread(read_messages, (s, ))
+    def __startpage(self, canvas : tkinter.Canvas):
+        ''' 
+            This method helps in rendering the starting page of the application.
+            Starting page is basically sending the name data to the server.
+        '''
 
+        canvas.delete("all")
+
+        label = tkinter.Label(self.screen, text = "Enter name : ")
+        canvas.create_window(300, 300, window = label)
+
+        name = tkinter.Entry(self.screen)
+        canvas.create_window(400, 300, window = name)
+
+        button = tkinter.Button(text = "Enter Chat room", command = lambda : self.__gotoChatroom(canvas, name.get()))
+        canvas.create_window(400, 350, window = button)
+
+    def __gotoChatroom(self, canvas : tkinter.Canvas, name : str):
+        '''
+            Guiding the GUI to clear the screen and render the chat room.
+            In the process, send the name data to the server.
+            Then, go to the chatroom page
+        '''
+
+        canvas.destroy()
+        self.name = name
+
+        print(self.name)
+        self.s.send(self.name.encode())
+
+        self.__chatroom()
+
+    def __showMessage(self, message : str):
+        '''
+            This is a utility function that displays the message in that chat
+            history.
+        '''
+
+        self.chats.config(state = tkinter.NORMAL)
+        self.chats.insert(tkinter.END, message + "\n")
+        self.chats.config(state = tkinter.DISABLED)
+
+    def __sendMessage(self, message : tkinter.Entry):
+        '''
+            This is a utility function which sends the data given by the user, 
+            and sends it to server.
+
+            Then, we clear the text field.
+        '''
+
+        _message = message.get()
+
+        # self.__showMessage(_message)
+        # print(_message)
+
+        self.s.send(_message.encode())
+
+        message.delete(0, tkinter.END)
+
+    def __chatroom(self):
+        '''
+            This is a chatroom.
+
+            It is divided into 2 frames.
+                1. Chat Frame
+                2. Message Frame
+
+            Chat Frame:-
+                In this frame, all the chat history is displayed from various users which is broadcasted
+                by the server.
+
+            Message Frame:-
+                In this frame, there is a text field for user input and a send button which transmits the data
+                to the server.
+        '''        
+
+        self.screen.geometry("800x600")
+
+        # ----------------x-------------------------
+
+        # Chat Frame :-
+        chatFrame = tkinter.Frame(self.screen)
+
+        yScroll =  tkinter.Scrollbar(chatFrame)
+        yScroll.pack(side = tkinter.RIGHT, fill = tkinter.Y)
+
+        self.chats = tkinter.Text(chatFrame, width = 100, height = 35, wrap = tkinter.WORD, yscrollcommand = yScroll.set)
+        yScroll.config(command = self.chats.yview)
+        self.chats.config(state = tkinter.DISABLED)
+        self.chats.pack()
+
+        chatFrame.pack(side = tkinter.TOP)
+
+        # ----------------x--------------------------
+
+        # Message Frame :-
+        messageFrame = tkinter.Frame(self.screen)
+
+        message = tkinter.Entry(messageFrame, width = 120)
+        message.pack(side = tkinter.LEFT)
+
+        sendButton = tkinter.Button(messageFrame, text = "Send", width = 10, command = lambda : self.__sendMessage(message))
+        sendButton.pack(side = tkinter.RIGHT)
+        
+        # ------------------x-------------------------
+
+        messageFrame.pack(side = tkinter.BOTTOM, fill = "x")
+
+    def read_messages(self, s: socket.socket):
+        '''
+            This function constantly reads if the server recieved 
+            any messages from other clients, if yes, it would be 
+            recieved and printed in the terminal/console.
+        '''
         while True:
 
-            send_message = input()
-            s.send(send_message.encode())
+            msg = s.recv(1024).decode()
+            self.__showMessage(msg)
+
+    def start_client(self):
+        '''
+            This function is responsible to connect this pc with
+            the server and be able to messages
+        '''
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.HOST, self.PORT))
+        threading._start_new_thread(self.read_messages, (self.s, ))
+
 
 if __name__ == "__main__":
-    start_client()
+
+    client = Client()
--- Server.py
+++ Server.py
@@ -2,81 +2,84 @@ import socket
 import threading
 
 # Parameters used in this program
-HOST = "192.168.20.103"
-PORT = 12345
 
-clients = []
-client_names = {}
+class Server:
 
-def broadcast_message(message : str):
-    '''
-        This function sends the message recieved 
-        from the client to all the other clients
-        to achieve multi-client Chat room
-    '''
-    _clients = clients.copy()
+    HOST = "192.168.20.103"
+    PORT = 12345
 
-    for c in _clients:
-        try:
+    clients = []
+    client_names = {}
+
+    def broadcast_message(self, message : str):
+        '''
+            This function sends the message recieved 
+            from the client to all the other clients
+            to achieve multi-client Chat room
+        '''
+        _clients = self.clients.copy()
+
+        for c in _clients:
             c.send(message.encode())
-        except Exception:
-            clients.remove(c)
-
-def listen_to_client_messages(conn : socket.socket):
-    '''
-        This function is ran under a new thread where it 
-        constantly listens to the ip if the client is sending
-        any messages and if any message is recieved the message
-        is broadcasted to other clients
-    '''
-
-    # Getting the IP and PORT of the Client
-    IP, PORT = conn.getpeername()
-
-    # The first message recieved by the client is the name
-    # of the client. This is mapped using the `client_names`
-    name = conn.recv(1024).decode()
-    # client_names[(IP, PORT)] = name
-
-    # Log to the server
-    print(f"{IP}:{PORT} => {name}")
-
-    # This Loop keeps on reading messages from the client
-    while True:
-
-        try:
-            msg = conn.recv(1024).decode()
-            broadcast_message(f"{name} => {msg}")
-
-        except Exception:
             
-            # client_names.pop((IP, PORT))
-            # clients.remove(conn)
-            if conn.close()
 
-            threading.currentThread()._stop()
+    def listen_to_client_messages(self, conn : socket.socket):
+        '''
+            This function is ran under a new thread where it 
+            constantly listens to the ip if the client is sending
+            any messages and if any message is recieved the message
+            is broadcasted to other clients
+        '''
 
+        # Getting the IP and PORT of the Client
+        IP, PORT = conn.getpeername()
 
-def start_server():
-    '''
-        This function is responsible to start the server
-        and listen to all the client messages using threads
-    '''
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # The first message recieved by the client is the name
+        # of the client. This is mapped using the `client_names`
+        name = conn.recv(1024).decode()
+        # client_names[(IP, PORT)] = name
 
-        s.bind((HOST, PORT))
-        print(f"Starting server on -> {HOST}:{PORT}")
-        
-        s.listen()
+        # Log to the server
+        print(f"{IP}:{PORT} => {name}")
 
+        # This Loop keeps on reading messages from the client
         while True:
 
-            conn, addr = s.accept()
-            
-            if conn not in clients:
-                clients.append(conn)
+            try:
+                msg = conn.recv(1024).decode()
+                self.broadcast_message(f"{name} => {msg}")
 
-            threading._start_new_thread(listen_to_client_messages, (conn, ))
+            except Exception:
+                
+                # client_names.pop((IP, PORT))
+                # clients.remove(conn)
+                
+                conn.close()
+                threading.currentThread()._stop()
+
+
+    def start_server(self):
+        '''
+            This function is responsible to start the server
+            and listen to all the client messages using threads
+        '''
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+            s.bind((self.HOST, self.PORT))
+            print(f"Starting server on -> {self.HOST}:{self.PORT}")
+            
+            s.listen()
+
+            while True:
+
+                conn, addr = s.accept()
+                
+                if conn not in self.clients:
+                    self.clients.append(conn)
+
+                threading._start_new_thread(self.listen_to_client_messages, (conn, ))
 
 if __name__ == "__main__":
-    start_server()
+
+    serv = Server()
+    serv.start_server()
